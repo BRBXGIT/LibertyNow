40b3ef7ea4091074ef8f069cbaf5f152b488ac8e
BRBX <brbxmain@gmail.com>
2025-06-30 22:05:08 +0300

    start creating player

---
+++ feature/player-screen/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    // Compose
+    alias(libs.plugins.kotlin.compose)
+    // Serialization
+    alias(libs.plugins.kotlin.serialization)
+    // Ksp
+    alias(libs.plugins.ksp)
+    // Hilt
+    alias(libs.plugins.hilt.android)
+}
+
+android {
+    namespace = "com.example.player_screen"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 28
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    // Core modules
+    implementation(project(":core:design-system"))
+
+    // Compose
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
+    // Material 3
+    implementation(libs.androidx.material3)
+    // Nav
+    implementation(libs.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+    // Animated icons(compose animation graphics)
+    implementation(libs.androidx.animation.graphics)
+    // Exo player
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.exoplayer.hls)
+    // System ui controller
+    implementation(libs.accompanist.systemuicontroller)
+}
