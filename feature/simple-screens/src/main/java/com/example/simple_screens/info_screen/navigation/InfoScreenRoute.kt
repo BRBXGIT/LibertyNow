b3c2f6615d3647c1c051556dad8d1989f4154236
BRBX <brbxmain@gmail.com>
2025-07-07 17:23:34 +0300

    find the last bug, now the release please!!!

--- feature/simple-screens/src/main/java/com/example/simple_screens/info_screen/navigation/InfoScreenRoute.kt
+++ feature/simple-screens/src/main/java/com/example/simple_screens/info_screen/navigation/InfoScreenRoute.kt
@@ -1,8 +1,12 @@
 package com.example.simple_screens.info_screen.navigation
 
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.example.design_system.theme.CommonConstants
 import com.example.simple_screens.info_screen.screen.InfoScreen
 import kotlinx.serialization.Serializable
 
@@ -11,6 +15,9 @@ object InfoScreenRoute
 
 fun NavGraphBuilder.infoScreen(
     navController: NavController
-) = composable<InfoScreenRoute> {
+) = composable<InfoScreenRoute>(
+    enterTransition = { fadeIn(tween(CommonConstants.ANIMATION_DURATION)) },
+    exitTransition = { fadeOut(tween(CommonConstants.ANIMATION_DURATION)) }
+) {
     InfoScreen(navController)
 }
